6f9890ea9f317087761a216cc0247cebbc2e1186
namzz <76516209+ImNamzz@users.noreply.github.com>
2025-08-25 04:24:22 +0700

    feat: map to levels. fixed: rounded font sizes

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 import pymupdf as pf
 
-pdf_path = "E:\QA_Document\data\CV.pdf"
+pdf_path = r"E:\QA_Document\data\CV.pdf"
+
+def font_level(pdf_path):
+    doc = pf.open(pdf_path)
+    font_sizes = set()
+    for page in doc:
+        blocks = page.get_text("dict")["blocks"]
+        for block in blocks:
+            if "lines" in block:
+                for line in block["lines"]:
+                    for span in line["spans"]:
+                        font_sizes.add(round(span["size"]))
+    doc.close()
+    #sort the font size descending
+    sort_sizes = sorted(list(font_sizes), reverse=True)
+    #map each font size to it's corresponding level
+    map_sizes_level = {}
+    for i, size in enumerate(sort_sizes):
+        map_sizes_level[size] = i
+
+    return map_sizes_level
+
 
 def extract_font_size(pdf_path):
 
@@ -23,5 +44,13 @@ def extract_font_size(pdf_path):
     return document_data
 
 extract_font_size(pdf_path)
+print(font_level(pdf_path))
 
 # version 1.0
+# Rules:
+# - I should treat larger font sizes as parents to smaller ones. In the end, the smallest will be the paragraph section.
+# List could also be heading
+# - Font styles could normally be used to emphasize, or sub titles
+# - Should somehow treat header and footer properly, they could cause noise for chunks
+# In this case, this shouldn't be treated like how normal pdf parser works anymore.
+# Using tree might work.
